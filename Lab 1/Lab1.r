19ee6a042236c7aa954e98520b9a0a6c6cece698
Drakunal <drakunaldey1@gmail.com>
2022-07-12 07:15:18 +0530

    basic plots added

--- Lab 1/Lab1.r
+++ Lab 1/Lab1.r
@@ -1,2 +1,30 @@
-setwd("C:\Users\Kunal\OneDrive\Documents\R\Data Visualization Lab\Lab 1")
+#setwd("give the path")
 airquality = read.csv('airquality.csv',header=TRUE, sep=",")
+
+#data exploration
+str(airquality)
+
+
+head(airquality)
+tail(airquality)
+
+summary(airquality)
+
+
+#some basic plots using the Base R functions
+plot(airquality$Ozone)
+
+
+#plot between ozone and wind value
+plot(airquality$Ozone, airquality$Wind) #shows negative correlation
+
+
+plot(airquality)
+
+#We get a matrix of scatterplots which is a correlation matrix of all the columns.
+#The plot above instantly shows that:
+  
+#  --The level of Ozone and Temperature is correlated positively.
+#  --Wind speed is negatively correlated to both Temperature and Ozone level.
+#We can quickly discover the relationship between variables by merely looking
+#at the plots drawn between them.
