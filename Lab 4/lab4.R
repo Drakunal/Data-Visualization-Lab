d7d952328170f2f1d941d2cc818ec1f4a6b87232
Drakunal <drakunaldey1@gmail.com>
2022-08-02 07:03:58 +0530

    heatmap added

--- Lab 4/lab4.R
+++ Lab 4/lab4.R
@@ -1,3 +1,5 @@
+library('tidyverse')
+
 #---------------------------------------------------------
 #Statistical Transformation
 #---------------------------------------------------------
@@ -34,3 +36,104 @@ ggplot(data = diamonds) +
     fun = median
   )
 #----------------------------------------------------
+#Heatmap
+#----------------------------------------------------
+#A heatmap is a graphical representation of data where
+#the individual values contained in a matrix are 
+#represented as colors. It is a bit like looking a 
+#data table from above.
+
+#USING THE HEATMAP() FUNCTION
+#The heatmap() function is natively provided in R. 
+#It produces high quality matrix and offers statistical 
+#tools to normalize input data, run clustering algorithm 
+#and visualize the result with dendrograms.
+
+
+
+#using ggplot2
+# Dummy data
+x <- LETTERS[1:20]
+y <- paste0("var", seq(1,20))
+data <- expand.grid(X=x, Y=y)
+data$Z <- runif(400, 0, 5)
+
+# Heatmap 
+ggplot(data, aes(X, Y, fill= Z)) + 
+  geom_tile()
+
+
+install.packages('hrbrthemes')
+library(hrbrthemes)
+
+# Dummy data
+x <- LETTERS[1:20]
+y <- paste0("var", seq(1,20))
+data <- expand.grid(X=x, Y=y)
+data$Z <- runif(400, 0, 5)
+
+# Give extreme colors:
+ggplot(data, aes(X, Y, fill= Z)) + 
+  geom_tile() +
+  scale_fill_gradient(low="white", high="blue") +
+  theme_ipsum()
+
+# Color Brewer palette
+ggplot(data, aes(X, Y, fill= Z)) + 
+  geom_tile() +
+  scale_fill_distiller(palette = "RdPu") +
+  theme_ipsum()
+
+# Color Brewer palette
+install.packages('viridis')
+library(viridis)
+ggplot(data, aes(X, Y, fill= Z)) + 
+  geom_tile() +
+  scale_fill_viridis(discrete=FALSE) +
+  theme_ipsum()
+
+
+
+# Volcano dataset
+#volcano
+
+# Heatmap 
+volcano %>%
+  
+  # Data wrangling
+  as_tibble() %>%
+  rowid_to_column(var="X") %>%
+  gather(key="Y", value="Z", -1) %>%
+  
+  # Change Y to numeric
+  mutate(Y=as.numeric(gsub("V","",Y))) %>%
+  
+  # Viz
+  ggplot(aes(X, Y, fill= Z)) + 
+  geom_tile() +
+  theme_ipsum() +
+  theme(legend.position="none")
+
+
+install.packages('plotly')
+library(plotly)
+# Dummy data
+x <- LETTERS[1:20]
+y <- paste0("var", seq(1,20))
+data <- expand.grid(X=x, Y=y)
+data$Z <- runif(400, 0, 5)
+
+# new column: text for tooltip:
+data <- data %>%
+  mutate(text = paste0("x: ", x, "\n", "y: ", y, "\n", "Value: ",round(Z,2), "\n", "What else?"))
+
+# classic ggplot, with text in aes
+p <- ggplot(data, aes(X, Y, fill= Z, text=text)) + 
+  geom_tile() +
+  theme_ipsum()
+
+ggplotly(p, tooltip="text")
+
+# save the widget
+# library(htmlwidgets)
+# saveWidget(pp, file=paste0( getwd(), "/HtmlWidget/ggplotlyHeatmap.html"))
