442e9c8b71764088e9bc982e4d07a6a3f6031e77
Drakunal <drakunaldey1@gmail.com>
2022-08-22 05:47:10 +0000

    data updated and label repel added

--- Lab 5/lab5.R
+++ Lab 5/lab5.R
@@ -2,12 +2,12 @@ library('tidyverse')
 
 names <- c("S1","S2","S3","S4","S5")
 hours_spent <- c(5,10,17,29,37)
-high_score <- c(100,200,250,370,510)
+high_score <- c(100,150,330,370,510)
 data <- data.frame(names, hours_spent, high_score)
 view(data)
 data2 <- tibble(names, hours_spent, high_score)
 view(data2)
-
+data
 ggplot(data, aes(hours_spent, high_score)) +
   geom_point() +
   geom_text(label = names)
@@ -17,5 +17,15 @@ ggplot(data, aes(hours_spent, high_score)) +
 ggplot(data, aes(hours_spent, high_score)) +
   geom_point() +
   geom_text(label = names, nudge_y = 20)
+
+# Install ggrepel package
+install.packages("ggrepel")
+library("ggrepel")
+
+ggplot(data, aes(hours_spent, high_score)) +
+  geom_point() +
+  geom_label_repel(label = names)
+
+?geom_label_repel
 
 #======================================================
